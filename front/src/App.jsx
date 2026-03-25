9a246f12f005b68092dcc8556a8285860dafcb89
Dezeli <haterecursive@gmail.com>
2026-03-25 13:28:40 +0900

    [Hotfix]: 데이터 로드 문제 완화

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -11,21 +11,15 @@ const PostDetail = lazy(() => import('./pages/PostDetail'))
 function App() {
     return (
         <BrowserRouter>
-            <Suspense fallback={
-                <div className="flex h-[50vh] items-center justify-center text-zinc-400">
-                    페이지를 불러오는 중입니다...
-                </div>
-            }>
-                <Routes>
-                    <Route path="/" element={<Layout />}>
-                        <Route index element={<Home />} />
-                        <Route path="projects" element={<ProjectList />} />
-                        <Route path="projects/:id" element={<ProjectDetail />} />
-                        <Route path="posts" element={<PostList />} />
-                        <Route path="posts/:id" element={<PostDetail />} />
-                    </Route>
-                </Routes>
-            </Suspense>
+            <Routes>
+                <Route path="/" element={<Layout />}>
+                    <Route index element={<Home />} />
+                    <Route path="projects" element={<ProjectList />} />
+                    <Route path="projects/:id" element={<ProjectDetail />} />
+                    <Route path="posts" element={<PostList />} />
+                    <Route path="posts/:id" element={<PostDetail />} />
+                </Route>
+            </Routes>
         </BrowserRouter>
     )
 }
