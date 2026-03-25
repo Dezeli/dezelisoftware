ebe00b46c2e7676ce15f3e298be52de1d6d22152
Dezeli <haterecursive@gmail.com>
2026-03-25 13:26:25 +0900

    [Hotfix]: 데이터 로드 문제 완화

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,24 +1,31 @@
-// front/src/App.jsx
+import React, { Suspense, lazy } from 'react'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
-import Home from './pages/Home'
-import ProjectList from './pages/ProjectList'
-import ProjectDetail from './pages/ProjectDetail'
-import PostList from './pages/PostList'
-import PostDetail from './pages/PostDetail'
+
+const Home = lazy(() => import('./pages/Home'))
+const ProjectList = lazy(() => import('./pages/ProjectList'))
+const ProjectDetail = lazy(() => import('./pages/ProjectDetail'))
+const PostList = lazy(() => import('./pages/PostList'))
+const PostDetail = lazy(() => import('./pages/PostDetail'))
 
 function App() {
     return (
         <BrowserRouter>
-            <Routes>
-                <Route path="/" element={<Layout />}>
-                    <Route index element={<Home />} />
-                    <Route path="projects" element={<ProjectList />} />
-                    <Route path="projects/:id" element={<ProjectDetail />} />
-                    <Route path="posts" element={<PostList />} />
-                    <Route path="posts/:id" element={<PostDetail />} />
-                </Route>
-            </Routes>
+            <Suspense fallback={
+                <div className="flex h-[50vh] items-center justify-center text-zinc-400">
+                    페이지를 불러오는 중입니다...
+                </div>
+            }>
+                <Routes>
+                    <Route path="/" element={<Layout />}>
+                        <Route index element={<Home />} />
+                        <Route path="projects" element={<ProjectList />} />
+                        <Route path="projects/:id" element={<ProjectDetail />} />
+                        <Route path="posts" element={<PostList />} />
+                        <Route path="posts/:id" element={<PostDetail />} />
+                    </Route>
+                </Routes>
+            </Suspense>
         </BrowserRouter>
     )
 }
